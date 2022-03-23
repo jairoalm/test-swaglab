846688be329851ecc244b5510a9385f839d96d35
Jairo Almeida <jairoam85@gmail.com>
2022-03-23 15:33:33 -0300

    Criando a logica do login

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,23 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('dataTest', value => {
+    return cy.get(`[data-test="${value}"]`)
+  })
+
+Cypress.Commands.add('login', (
+    user = Cypress.env('user'),
+    password = Cypress.env('password')
+) => {
+    cy.dataTest('username').type(user)
+    cy.dataTest('password').type(password), {log: false}
+    cy.dataTest('login-button').click()
+})
+
+Cypress.Commands.add('checkErrorMsg', msg => {
+    cy.dataTest('error')
+      .should('be.visible')
+      .and('contain', msg)
+  })
+  
+Cypress.Commands.add('programmaticLogin', () => {
+    cy.setCookie('session-username', Cypress.env('user'))
+    cy.visit('/inventory.html', { failOnStatusCode: false })
+  })
